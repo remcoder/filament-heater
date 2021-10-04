d1db9f5b84e9d797f38430fdea6479644e3f4b7d
Remco Veldkamp <remcoder@gmail.com>
2021-10-04 20:16:32 +0200

    initial commit

---
+++ main.ino
@@ -0,0 +1,215 @@
+#include <ESP32AnalogRead.h>
+
+#include <SimpleDHT.h>
+
+#include <U8g2lib.h>
+#include <U8x8lib.h>
+
+
+/*
+LCD12864 (ST7920 128X64)
+ - clock: 18 
+ - data: 23
+ - CS: 5
+*/
+U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 18, /* data=*/ 23, /* CS=*/ 5, /* reset=*/ 22); // ESP32
+
+/*
+Relay (digital out)
+ - 21
+*/
+int pinHeaterRelay = GPIO_NUM_21;
+
+/*
+DHT22 (digital in/out)
+ - 27
+*/
+int pinDHT22 = GPIO_NUM_33;
+SimpleDHT22 dht22(pinDHT22);
+
+/*
+Bed thermistor (analog)
+- 
+*/
+ESP32AnalogRead adc;
+int pinBedThermistor = A6; // GPIO 14
+
+
+bool isHeaterRelayOn = false;
+int bedTemp = -1;
+int currentTemp = -1;
+int currentHumidity = -1;
+int targetTemp = 40;
+int targetOffset = 5; // seems to result in ±5 degree overshoot
+int bedTempMaxDiff = 15;
+int THERMAL_RUNAWAY_TIMOUT = 6; //seconds
+int initialTemp = -1;
+int fontBBXHeight = 11;
+int lineHeight = fontBBXHeight;
+
+void(* resetFunc) (void) = 0;
+
+void gotoLine(int line) {
+  int y = (line+1) * lineHeight;
+  u8g2.setCursor(0, y);
+}
+
+void updateScreen(int temp, int hum) {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_t0_11_mf); // w=6, h=11, 
+  gotoLine(0);
+  u8g2.print("current: ");
+  u8g2.print(temp);
+  u8g2.print("C, ");
+
+  u8g2.print(hum);
+  u8g2.print("H%");
+
+  gotoLine(1);
+  u8g2.print("bed temp: ");
+  u8g2.print(bedTemp);
+  u8g2.print("C");
+
+  gotoLine(2);
+  u8g2.print("target temp: ");
+  u8g2.print(targetTemp);
+  u8g2.print("C");
+
+
+  if (isHeaterRelayOn) {
+    gotoLine(4);
+    u8g2.print("HEATING! ");
+  }
+
+  u8g2.sendBuffer();
+}
+
+void turnOnHeater(bool state) {
+  digitalWrite(pinHeaterRelay, state);
+}
+
+void updateHeater() {
+  isHeaterRelayOn = (currentTemp < (targetTemp - targetOffset)) && bedTemp < (targetTemp + bedTempMaxDiff);
+  turnOnHeater(isHeaterRelayOn);
+}
+
+void measureTemp() {
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht22.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT22 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(2000);
+    return;
+  }
+  
+  Serial.print("Sample OK: ");
+  currentTemp = (int)temperature;
+  currentHumidity = (int)humidity;
+  Serial.print(currentTemp); Serial.print(" *C, ");
+  Serial.print(currentHumidity); Serial.println(" RH%");
+}
+
+void getBedTemp()
+{
+
+    // Converts input from a thermistor voltage divider to a temperature value.
+    // The voltage divider consists of thermistor Rt and series resistor R0.
+    // The value of R0 is equal to the thermistor resistance at T0.
+    // You must set the following constants:
+    //                  adcMax  ( ADC full range value )
+    //                  analogPin (Arduino analog input pin)
+    //                  invBeta  (inverse of the thermistor Beta value supplied by manufacturer).
+    // Use Arduino's default reference voltage (5V or 3.3V) with this module.
+    //
+
+  const float invBeta = 1.00 / 3980.00;   // replace "Beta" with beta of thermistor
+
+  const float adcMax = 4095.00;
+  const float invT0 = 1.00 / 298.15;   // room temp in Kelvin
+
+  int adcVal, i, numSamples = 100;
+  float  K, C, F;
+
+  adcVal = 0;
+  for (i = 0; i < numSamples; i++)
+   {
+     adcVal = adcVal + analogRead(pinBedThermistor);
+     delay(1);
+   }
+  adcVal = adcVal/numSamples;
+  K = 1.00 / (invT0 + invBeta*(log ( adcMax / (float) adcVal - 1.00)));
+  C = K - 273.15;                      // convert to Celsius
+  //F = ((9.0*C)/5.00) + 32.00;   // convert to Fahrenheit
+  
+  if (bedTemp == -1)
+    bedTemp = C;
+  else
+    // dampen the changes
+    bedTemp = (int)((float)bedTemp * 0.8 + C * 0.2);
+}
+
+void checkForThermalRunaway() {
+  if (initialTemp == -1) {
+    initialTemp = bedTemp;
+  }
+
+  if (millis() > (THERMAL_RUNAWAY_TIMOUT * 1000) ){
+    if (bedTemp == initialTemp) {
+      turnOnHeater(false);
+      haltOnError("THERMAL RUNAWAY");
+    }
+  }
+}
+
+void haltOnError(String error) {
+  Serial.println("ERROR" + error);
+  
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_t0_11_mf); // w=6, h=11, 
+  gotoLine(0);
+  u8g2.print("ERROR" + error);
+  u8g2.sendBuffer();
+
+  while (true) {
+    delay(1000);
+  }
+}
+
+void setup()
+{
+  adc.attach(pinBedThermistor);
+
+  pinMode(pinHeaterRelay, OUTPUT);
+
+  u8g2.begin();
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  u8g2.drawStr(0,20,"Hello World!");
+  u8g2.sendBuffer();
+	Serial.begin(115200);
+}
+
+void loop() {
+  // start working...
+  Serial.println("=================================");
+  Serial.println("Sample DHT22...");
+  
+  measureTemp();
+
+  Serial.println("=================================");
+  Serial.println("Updating screen...");
+  
+  getBedTemp();
+
+  checkForThermalRunaway();
+
+  updateHeater();
+
+  updateScreen(currentTemp, currentHumidity);
+  
+  // DHT22 sampling rate is 0.5HZ.
+  delay(500);
+}
+
